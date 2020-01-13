73eb3ff38c684aacc5c2f211f90cd25bb94fca4c
Cloud <117976509@qq.com>
2020-01-13 12:55:21 +0800

    gocmd以个人常用的库工具的形式拿出来新建一个开源库

--- controllers/newapp.go
+++ controllers/newapp.go
@@ -1,15 +1,21 @@
 package controllers
 
+import (
+	"gitee.com/griffin702/services"
+	"gocmd/models"
+)
+
 var (
 	App = newApp()
 )
 
-func newApp() (app *CmdGo) {
-	app = &CmdGo{
-		ParamList: map[string]string{
-			"-a": "",
-			"-v": "",
-		},
-	}
+func newApp() (app *services.CmdGo) {
+	app = services.New()
+	app.RegistAction([]services.Action{
+		new(models.KickAction),
+		new(models.SaveAction),
+		new(models.CloseAction),
+		new(models.HotAction),
+	})
 	return
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module gocmd
+
+go 1.13
+
+require gitee.com/griffin702/services v1.0.0
